b3eaf256a3227ed3f42d42989d613498da75ee36
Fran Moretti <franciscohmoretti@gmail.com>
2022-09-09 19:23:15 -0300

    Se incorpora editar/eliminar y el gitignore

--- AppCoder/ulrs.py
+++ AppCoder/ulrs.py
@@ -3,18 +3,27 @@ from django.urls import path
 from AppCoder import views
 
 urlpatterns = [
+    #Barra inicial
     path('', views.inicio, name="inicio"),
     path('partido/', views.partido, name="partidos"),
     path('jugadores/', views.jugador, name="jugadores"),
     path('reservas/', views.reservas, name="reservas"),
-    path('partidos/', views.partido),
+    #Reservas
     path('crear-reserva/', views.crear_reserva, name="crear_reserva"),
     path('busqueda-reserva-form/', views.busqueda_reservas, name="busqueda_reserva_form"),
     path('busqueda-reserva/', views.buscar_reserva, name="busqueda_reserva"),
+    path('editar-reserva/<int:id>/', views.editar_reserva, name="editar_reserva"),
+    path('eliminar-reserva/<int:id>/', views.eliminar_reserva, name="eliminar_reserva"),
+    #Jugadores
     path('crear-jugador/', views.crear_jugador, name="crear_jugador"),
     path('busqueda-jugador-form/', views.busqueda_jugador, name="busqueda_jugador_form"),
     path('busqueda-jugador/', views.buscar_jugador, name="busqueda_jugador"),
+    path('editar-jugador/<int:id>/', views.editar_jugador, name="editar_jugador"),
+    path('eliminar-jugador/<int:id>/', views.eliminar_jugador, name="eliminar_jugador"),
     path('registrar-partido/', views.crear_partido, name="crear_partido"),
+    #Partidos
     path('busqueda-partido-form/', views.busqueda_partido, name="busqueda_partido_form"),
     path('busqueda-partido/', views.buscar_partido, name="busqueda_partido"),
+    path('editar-partido/<int:id>/', views.editar_partido, name="editar_partido"),
+    path('eliminar-partido/<int:id>/', views.eliminar_partido, name="eliminar_partido"),
 ]
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -1,12 +1,11 @@
 from datetime import date
 from typing import Dict
 from django.shortcuts import HttpResponse
-from django.shortcuts import render, redirect, reverse
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from AppCoder.models import Partido, Reserva, Jugador
 from AppCoder.forms import ReservaFormulario, JugadorFormulario, PartidoFormulario
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 
 def inicio(request):
@@ -26,7 +25,7 @@ def jugador(request):
     jugadores = Jugador.objects.all()
     return render(request, "AppCoder/jugadores.html", {'jugadores': jugadores})
 
-# Fromulario Reservas
+# Formulario Reservas
 
 def crear_reserva(request):
     if request.method == 'POST':
@@ -53,6 +52,42 @@ def buscar_reserva(request):
         return render(request, "AppCoder/reservas.html", {'reservas': reservas})
     else:
         return render(request, "AppCoder/reservas.html", {'reservas': []})
+
+def editar_reserva(request, id):
+    reserva = Reserva.objects.get(id=id)
+
+    if request.method == 'POST':
+        formulario = ReservaFormulario(request.POST)
+
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+
+            reserva.nombre = data['nombre']
+            reserva.fecha = data['fecha']
+            reserva.equipo = data['equipo']
+            reserva.email = data['email']
+
+            reserva.save()
+
+            return redirect(reverse('reservas'))
+    else:  # GET
+        inicial = {
+            'nombre': reserva.nombre,
+            'fecha': reserva.fecha,
+            'equipo': reserva.equipo,
+            'email': reserva.email,            
+        }
+        formulario = ReservaFormulario(initial=inicial)
+    return render(request, "AppCoder/form_reserva.html", {"formulario": formulario})
+
+def eliminar_reserva(request, id):
+    reserva = Reserva.objects.get(id=id)
+    borrado_id = reserva.id
+    reserva.delete()
+    url_final = f"{reverse('reservas')}?borrado={borrado_id}"
+
+    return redirect(url_final)
+    
 
 #Formulario Juagdor
 
@@ -81,6 +116,42 @@ def buscar_jugador(request):
     else:
         return render(request, "AppCoder/jugadores.html", {'jugadores': []})
 
+def editar_jugador(request, id):
+    jugador = Jugador.objects.get(id=id)
+
+    if request.method == 'POST':
+        formulario = JugadorFormulario(request.POST)
+
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+
+            jugador.nombre = data['nombre']
+            jugador.apellido = data['apellido']
+            jugador.posicion = data['posicion']
+            jugador.pais = data['pais']
+
+            jugador.save()
+
+            return redirect(reverse('jugadores'))
+    else:  # GET
+        inicial = {
+            'nombre': jugador.nombre,
+            'apellido': jugador.apellido,
+            'posicion': jugador.posicion,
+            'pais': jugador.pais,
+        }
+        formulario = JugadorFormulario(initial=inicial)
+    return render(request, "AppCoder/form_jugador.html", {"formulario": formulario})
+
+def eliminar_jugador(request, id):
+    jugador = Jugador.objects.get(id=id)
+    borrado_id = jugador.id
+    jugador.delete()
+    url_final = f"{reverse('jugadores')}?borrado={borrado_id}"
+
+    return redirect(url_final)
+
+
 #Formulario de Partido
 
 def crear_partido(request):
@@ -108,5 +179,35 @@ def buscar_partido(request):
     else:
         return render(request, "AppCoder/partido.html", {'partidos': []})
 
+def editar_partido(request, id):
+    partido = Partido.objects.get(id=id)
+
+    if request.method == 'POST':
+        formulario = PartidoFormulario(request.POST)
+
+        if formulario.is_valid():
+            data = formulario.cleaned_data
+
+            partido.fecha = data['fecha']
+            partido.equipo = data['equipo']
+
+            partido.save()
+
+            return redirect(reverse('partidos'))
+    else:  # GET
+        inicial = {
+            'fecha': partido.fecha,
+            'equipo': partido.equipo,
+        }
+        formulario = PartidoFormulario(initial=inicial)
+    return render(request, "AppCoder/form_partido.html", {"formulario": formulario})
+
+def eliminar_partido(request, id):
+    partido = Partido.objects.get(id=id)
+    borrado_id = partido.id
+    partido.delete()
+    url_final = f"{reverse('partidos')}?borrado={borrado_id}"
+
+    return redirect(url_final)
 
 
